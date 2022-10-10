3e7912f7d9a1a8413a8a7a3ed464034d40511748
Fellipe Marques <104071374+FellipeMSC@users.noreply.github.com>
2022-10-10 17:18:39 -0300

    Create funcoes.hpp

---
+++ Pilha/Lanchonete Delivery/funcoes.hpp
@@ -0,0 +1,24 @@
+#ifndef Pilha_h
+#define Pilha_h
+#include <stack>
+
+using namespace std;
+
+#define MAXTAM 10
+#define MAX_ENTREGA 7
+
+typedef struct {
+int codigo;
+int produtos [ MAXTAM ];
+float valor_pedido = 0.0;
+float distancia = 0.0;
+int TamanhoProduto = 0;
+}Pedido;
+
+void Menu();
+void Cardapio();
+void PesquisaItem(stack <Pedido> pesquisa, int id, string nome[], float preco[]);
+void OrdenarPedido(stack <Pedido> *ordem, string nome[], float preco[]);
+void LancarEntrega(stack <Pedido> *PilhaPedido);
+
+#endif
